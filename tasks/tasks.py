0bbb17ab258cbe3c9092a6ac69bdfe18bd18eb54
EniolaAdemola <it.eniolaademola@gmail.com>
2025-03-11 00:51:33 -0700

    Refactor task definitions in tasks.py for improved clarity and output customization

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,26 +1,26 @@
 from crewai import Task
 from tools.tools import yt_tool
-from agent.agent import blog_researcher, blog_writer
+from agent.agents import blog_researcher,blog_writer
 
-# Research Task
+## Research Task
 research_task = Task(
-    description = (
-        "Identify the video {topic}"
-        "Get detailed information about the video from the channel video"
-    ),
-    expected_output = "A comprehensive report based on the {topic} of the video content.",
-    tools = [yt_tool],
-    agent = blog_researcher
+  description=(
+    "Identify the video {topic}."
+    "Get detailed information about the video from the channel video."
+  ),
+  expected_output='A comprehensive 3 paragraphs long report based on the {topic} of video content.',
+  tools=[yt_tool],
+  agent=blog_researcher,
 )
 
-# Writing Task
+# Writing task with language model configuration
 write_task = Task(
-    description = (
-        "Get the information from the youtube channel on the topic {topic}"
-    ),
-    expected_output = "Summarize the information from the youtube channel video on the topic {topic} and create the content for the blog post.",
-    tools = [yt_tool],
-    agent = blog_writer,
-    async_execution = False,
-    output_file = "blog-post.md" # save the output to the file
+  description=(
+    "get the info from the youtube channel on the topic {topic}."
+  ),
+  expected_output='Summarize the info from the youtube channel video on the topic{topic} and create the content for the blog',
+  tools=[yt_tool],
+  agent=blog_writer,
+  async_execution=False, # Example of async task execution
+  output_file='new-blog-post.md'  # Example of output customization
 )
